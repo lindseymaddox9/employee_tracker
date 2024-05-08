28c0d5fa6bb5a953b83df421d66a50ae406f827a
lindseymaddox9 <153481027+lindseymaddox9@users.noreply.github.com>
2024-05-08 09:30:04 -0500

    attempt at joins

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ function start(){
         else if(answer.prompt === "Show All Roles"){
             viewRoles()
         }
-        // et al.
-
     })
 }
 
@@ -37,37 +35,62 @@ function start(){
 function viewDepartments(){
     connection.query("SELECT * FROM department", (err, res) => {
         if (err) throw err;
-        /// 
+        ///error viewing department
         console.table(res.rows)
         start()
     })
-}
+};
 
 function viewDepartments(){
-    connection.query("SELECT * FROM department", (err, res) => {
+    connection.query("SELECT * FROM role", (err, res) => {
         if (err) throw err;
 
         console.log(res.rows)
         start()
     })
-}
+};
 
 function viewDepartments(){
-    connection.query("SELECT * FROM department", (err, res) => {
+    connection.query("SELECT * FROM employee", (err, res) => {
         if (err) throw err;
 
         console.log(res.rows)
         start()
     })
-}
+};
 
+//join the role and department tables to get back the department name and the role details.
+// function viewRoles() {
+//     const query =`
+//     SELECTrole.id, role.title, role.salary, department.department_name
+//     FROM role 
+//     JOIN department ON role.department_id=department.id
+//     `;
+//     connection.query( query, (err, res) => {
+//         if (err) throw err;
+//         console.table(res, rows);
+//         start();
+//     })
+// }
 
+//join the employee data, role title, department name, 
+//salary, and manager's first and last names.
 
-
-
-
-start()
-
-
+// function viewEmployees() {
+//     const query = `
+//       SELECT emplyee.id, employee.first_name, employee.last_name, role.title, 
+//       department.department_name, role.salary, manager.first_name AS manager_first_name,
+//       manager.last_name AS manager_last_name
+//       FROM employee 
+//       LEFT JOIN role r ON employee.role_id = role.id
+//       LEFT JOIN department department ON role.department_id = department.id
+//       LEFT JOIN employee m ON emplyee.manager_id = manager.id
+//     `;
+//     connection.query(query, (err, res) => {
+//       if (err) throw err;
+//       console.table(res.rows);
+//       start();
+//     });
+//   }
 
 
